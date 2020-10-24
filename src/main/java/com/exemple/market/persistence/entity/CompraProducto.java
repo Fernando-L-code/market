c8a86d9fa69f6d8c5a18a4ce9f42f61eba1fbed4
Luis Fernando <f.er1998@hotmail.com>
2020-10-23 23:34:53 -0500

    Entity generando mapeo de la base de datos

--- src/main/java/com/exemple/market/persistence/entity/CompraProducto.java
+++ src/main/java/com/exemple/market/persistence/entity/CompraProducto.java
@@ -1,9 +1,6 @@
 package com.exemple.market.persistence.entity;
 
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "compras_productos")
@@ -15,6 +12,14 @@ public class CompraProducto {
     private Integer cantidad;
     private Double total;
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra",insertable = false,updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto",insertable = false,updatable = false)
+    private Producto producto;
 
     public CompraProductoPK getId() {
         return id;
